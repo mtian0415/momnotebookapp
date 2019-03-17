381491a56d84afb4f87050dd33af290acc891cbf
mtian0415 <tmwj1001@gmail.com>
2019-03-16 17:08:52 -0700

    initial try

---
+++ momnote/src/calendar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
+
+
+export default class MyApp extends Component {
+  // state = {
+  //   date: new Date(),
+  // }
+
+  //onChange = date => this.setState({ date })
+  //onClick= date => alert('click tester');;
+  render() {
+    return (
+      <div>
+      <Calendar
+        //  onChange={this.onChange}
+        //  value={this.state.date}
+      />
+      </div>
+    );
+  }
+}
---
+++ momnote/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MyApp from './calendar.js';
+
+ReactDOM.render(<MyApp />, document.getElementById("root"));
