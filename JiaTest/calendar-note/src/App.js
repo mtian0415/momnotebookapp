2aff46ff5b16fa2857fc7212adb09a6b296fe53b
Jia Wang <jiwana@microsoft.com>
2019-03-17 11:45:43 -0700

    [JiaTest] Added CodePen notes sample and use it in calendar-note code

--- JiaTest/calendar-note/src/App.js
+++ JiaTest/calendar-note/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import Calendar from 'react-calendar';
-import Note from './Note.js';
+import Form from './Form.js';
 
 class App extends Component {
-  state =
+  constructor(props)
   {
-    date: new Date(),
+    super(props);
+    this.state =
+    {
+      today: new Date(),
+      notes: []
+    };
+
+    this.addNote = this.addNote.bind(this);
   }
 
   onChange()
@@ -16,18 +23,26 @@ class App extends Component {
 
   onClickDay(value)
   {
-    alert('Day: ' + value);
+    this.setState({today:value});
+  }
+
+  addNote()
+  {
+    alert('add note');
   }
 
   render() {
+    const noteContent =
+      <button onClick={this.addNote}> Add Note </button>;
+
     return (
       <div>
         <Calendar
           onChange={()=>this.onChange()}
           onClickDay={(value)=>this.onClickDay(value)}
-          value={this.state.date}
+          value={this.state.today}
           />
-        <Note />
+        <div>{noteContent}</div>
 
       </div>
     );
