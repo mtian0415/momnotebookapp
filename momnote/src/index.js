500909f9c6d8007305b43fc8aaae9cf6034f5dfd
mtian0415 <tmwj1001@gmail.com>
2019-03-24 22:47:59 -0700

    dddd

--- momnote/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import MyApp from './calendar.js';
-
-ReactDOM.render(<MyApp />, document.getElementById("root"));
